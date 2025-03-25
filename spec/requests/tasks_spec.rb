0bda563b96ee619671e5c89ecd2bbcd219cff561
fujiikei <azyuyu@gmail.com>
2025-03-25 21:43:20 +0900

    rspec_task_create

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe "Tasks", type: :request do
       expect(response.body).to include(task.title)
     end
   end
+  describe "POST /tasks" do
+    it "新しいタスクを作成できる" do
+      task_params = { task: { title: "新しいタスク" } }
+      
+      post tasks_path, params: task_params
+      
+      expect(response).to have_http_status(:created)
+      expect(Task.last.title).to eq("新しいタスク")
+    end
+  end
 end
