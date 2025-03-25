a9908b1c70def60a121d1ca25c183da417680ead
fujiikei <azyuyu@gmail.com>
2025-03-25 21:24:45 +0900

    rspec_task_model

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "タイトルがあれば有効である" do
+    task = Task.new(title: "サンプルタスク")
+    expect(task).to be_valid
+  end
+
+  it "タイトルがないと無効である" do
+    task = Task.new(title: nil)
+    expect(task).to be_invalid
+  end
 end
