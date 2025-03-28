4e67fd98b1e6490984c349ad60e2049cc73b48cf
fujiikei <azyuyu@gmail.com>
2025-03-28 16:47:46 +0900

    add_factorybot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :user do
+    email { "test@test.com" }
+    password { "tttttt" }
+    password_confirmation { "tttttt" }
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "POST /users/sign_in" do
-    let(:user) { User.create!(email: "test@test.com", password: "tttttt", password_confirmation: "tttttt") }
+    let(:user) { create(:user) }
 
     it "ユーザーがログインできる" do
-      post user_session_path, params: { user: { email: user.email, password: "tttttt" } }
-      
+      post user_session_path, params: { user: { email: user.email, password: user.password } }
+
       expect(response).to have_http_status(:see_other)
     end
   end
